58dcd048065f69e096a98101e66a4ba39e88ba61
Gloridust <Gloridust@gmail.com>
2022-02-18 20:46:50 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,38 +1,48 @@
-export const title = "CFLSGX团学会";
-export const root = "https://dev.cflsgx.top";
-export const description = "成都外国语学校高新校区团学会官网。";
-export const keywords = ["成外高新团学会", "成高团学会", "成都外国语学校高新校区", "成外高新"];
-export const author = {
-	name: "成外高新团学会",
-	image: "/public/image/author.jpg",
-	twitter: "https://twitter.com/rea1DonandTrump",
-	github: "https://github.com/RiverTwilight",
-	intro: [
-		{
-			title: "关于作者",
-			content: "本站由成外高新团学会开发与运营。",
+module.exports = {
+	title: "CFLSGX团学会",
+	root: "https://dev.cflsgx.top",
+	description: "成都外国语学校高新校区团学会官网。",
+	keywords: ["成外高新团学会","成高团学会","成都外国语学校高新校区","成外高新"],
+	author: {
+		name: "成外高新团学会",
+		image: "/public/image/author.jpg",
+		twitter: "https://twitter.com/rea1DonandTrump",
+		github: "https://github.com/RiverTwilight",
+		intro: [
+			{
+				title: "关于作者",
+				content:
+					"本站由成外高新团学会开发与运营。",
+			},
+		],
+	},
+	categories: {
+		all: {
+			"en-US": "All Posts",
+			"zh-CN": "全部",
 		},
-	],
-};
-export const categories = {
-	all: {
-		"en-US": "All Posts",
-		"zh-CN": "全部",
+		Life: {
+			"en-US": "Life",
+			"zh-CN": "生活",
+		},
+		FrontEnd: {
+			"en-US": "Front End",
+			"zh-CN": "前端",
+		},
+		Feeling: {
+			"en-US": "Feeling",
+			"zh-CN": "随感",
+		},
+		SoftWare: {
+			"en-US": "SofeWare",
+			"zh-CN": "软件开发",
+		},
 	},
-	Life: {
-		"en-US": "Life",
-		"zh-CN": "生活",
-	},
-	FrontEnd: {
-		"en-US": "Front End",
-		"zh-CN": "前端",
-	},
-	Feeling: {
-		"en-US": "Feeling",
-		"zh-CN": "随感",
-	},
-	SoftWare: {
-		"en-US": "SofeWare",
-		"zh-CN": "软件开发",
-	},
+	// gitalk: {
+	// 	clientID: "ecaa627dee7516573aed",
+	// 	clientSecret: "03c76d614cc2bc51f7773dffed8a9ae17131e4f7",
+	// 	repo: "Blog-Comment",
+	// 	owner: "RiverTwilight",
+	// 	admin: ["RiverTwilight"],
+	// },
 };
