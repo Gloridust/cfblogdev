3083ce205f5b6b4d665692ded68c06b0ced96a14
Gloridust <Gloridust@gmail.com>
2022-02-18 21:58:10 +0800

    Update index.tsx

--- components/Drawer/index.tsx
+++ components/Drawer/index.tsx
@@ -97,24 +97,15 @@ export default class extends React.Component<DrawerProps, DrawerState> {
 								},
 							]}
 						/>
-						<div>
-							Made With
-							<LoveIcon
-								viewBox="0 0 24 24"
-								aria-hidden="true"
-								focusable="false"
-								fill="currentColor"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path fill="none" d="M0 0h24v24H0z"></path>
-								<path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"></path>
-							</LoveIcon>
-							by 江村暮
+						<div><p>
+							Developed by <a href="https://www.rene.wang/">江村暮</a><br/>
+							Branch developed by <a href="https://gloridust.tk/">辉晨Gloridust</a> <br/>
+							Managed by CFLSGX团学会</p>
 						</div>
 						<div>
-							本网站代码完全<a href="https://github.com/rivertwilight/NBlog">
+							本网站代码完全<a href="https://github.com/Gloridust/cfblogdev">
                                 <u>开源</u>
-							</a>
+							</a>,仅供学习交流使用
 						</div>
 					</div>
 				</div>
